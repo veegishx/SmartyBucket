ed8ceb7d5beb065cb0ab88388f18498871438a91
Veegish Ramdani <veegish@cyberstorm.mu>
2019-09-21 13:03:21 +0400

    Replaced bottom navigation with BottomAppBar component
    Signed-off-by: Veegish Ramdani <veegish@cyberstorm.mu>

--- app/src/main/java/com/saphyrelabs/smartybucket/MainActivity.java
+++ app/src/main/java/com/saphyrelabs/smartybucket/MainActivity.java
@@ -1,7 +1,13 @@
 package com.saphyrelabs.smartybucket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize BottomAppBar
+        BottomAppBar bab = findViewById(R.id.bottom_app_bar);
+
+        // Append menu items to BottomAppBar
+        bab.replaceMenu(R.menu.menu_items);
+
+        // Handle onClick event
+        bab.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.app_bar_search:
+                        Toast.makeText(MainActivity.this,"Like Clicked",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.app_bar_inventory:
+                        Toast.makeText(MainActivity.this,"Search Clicked",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.app_bar_settings:
+                        Toast.makeText(MainActivity.this,"Settings Clicked",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+
+        });
     }
 }
