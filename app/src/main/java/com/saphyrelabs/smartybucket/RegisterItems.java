d25c4305ff079e0dd508c251add79147eaa46323
Veegish Ramdani <veegish@cyberstorm.mu>
2019-09-27 18:01:31 +0400

    Improved register_items view
    Signed-off-by: Veegish Ramdani <veegish240@gmail.com>

--- app/src/main/java/com/saphyrelabs/smartybucket/RegisterItems.java
+++ app/src/main/java/com/saphyrelabs/smartybucket/RegisterItems.java
@@ -3,6 +3,8 @@ package com.saphyrelabs.smartybucket;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class RegisterItems extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class RegisterItems extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_items);
+        Spinner spinner = (Spinner) findViewById(R.id.items_category_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.items_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 }
