a46dc15d1ac6e2e9d3b12eb897075a6b7bc66c59
Veegish Ramdani <veegish@cyberstorm.mu>
2019-10-06 13:00:22 +0400

    Improved styling for Spinner and edittext
    Signed-off-by: Veegish Ramdani <veegish240@gmail.com>

--- app/src/main/java/com/saphyrelabs/smartybucket/RegisterItems.java
+++ app/src/main/java/com/saphyrelabs/smartybucket/RegisterItems.java
@@ -12,13 +12,14 @@ public class RegisterItems extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_items);
-//        Spinner spinner = (Spinner) findViewById(R.id.items_category_spinner);
-//        // Create an ArrayAdapter using the string array and a default spinner layout
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//                R.array.items_array, android.R.layout.simple_spinner_item);
-//        // Specify the layout to use when the list of choices appears
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        // Apply the adapter to the spinner
-//        spinner.setAdapter(adapter);
+        Spinner spinner = (Spinner) findViewById(R.id.items_category_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.item_categories_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
     }
 }
