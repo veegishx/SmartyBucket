d4b39044da5eddf9ebce8c3713e9d2eb20dcddc1
Veegish Ramdani <veegish@cyberstorm.mu>
2020-01-05 13:24:54 +0400

    Implemented account logout feature
    Signed-off-by: Veegish Ramdani <veegish@cyberstorm.mu>

--- app/src/main/java/com/saphyrelabs/smartybucket/MyProfile.java
+++ app/src/main/java/com/saphyrelabs/smartybucket/MyProfile.java
@@ -2,12 +2,23 @@ package com.saphyrelabs.smartybucket;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.login.LoginManager;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MyProfile extends AppCompatActivity {
     private TextView facebookAccountName;
+    private Button logoutBtn;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +29,35 @@ public class MyProfile extends AppCompatActivity {
 
         facebookAccountName = (TextView) findViewById(R.id.accountName);
         facebookAccountName.setText(facebookName);
+
+        logoutBtn = (Button) findViewById(R.id.logoutBtn);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                LoginManager.getInstance().logOut();
+                updateUI();
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser ==null) {
+            updateUI();
+        }
+    }
+
+    public void updateUI() {
+        Intent mainActivity  = new Intent(MyProfile.this, SignIn.class);
+        startActivity(mainActivity);
+        Toast.makeText(this, "You have been logged out", Toast.LENGTH_LONG).show();
     }
 }
