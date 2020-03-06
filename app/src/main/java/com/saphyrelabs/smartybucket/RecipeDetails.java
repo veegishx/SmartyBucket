34d9f32c8c62b418984e8145415d80b4ff94d0e7
veegishx <veegish240@gmail.com>
2020-03-06 19:09:44 +0400

    Added WebView to load and display recipes from API

--- app/src/main/java/com/saphyrelabs/smartybucket/RecipeDetails.java
+++ app/src/main/java/com/saphyrelabs/smartybucket/RecipeDetails.java
@@ -1,14 +1,86 @@
 package com.saphyrelabs.smartybucket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class RecipeDetails extends AppCompatActivity {
+
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+public class RecipeDetails extends AppCompatActivity implements AppBarLayout.OnOffsetChangedListener {
+
+    private ImageView imageView;
+    private TextView appbar_title, appbar_subtitle, title;
+    private boolean hiddenToolbarView = false;
+    private LinearLayout titleAppBar;
+    private AppBarLayout appBarLayout;
+    private Toolbar toolbar;
+    private String mUrl, mImg, mTitle, mSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe_details);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar();
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle("");
+
+        appBarLayout = findViewById(R.id.appbar);
+        titleAppBar = findViewById(R.id.title_appbar);
+        imageView = findViewById(R.id.backdrop);
+        appbar_title = findViewById(R.id.title_on_appbar);
+        appbar_subtitle = findViewById(R.id.subtitle_on_appbar);
+        title = findViewById(R.id.title);
+    }
+
+    private void initWebView(String url) {
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setSupportZoom(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int i) {
+        int maxScroll = appBarLayout.getTotalScrollRange();
+        float percentage = (float) Math.abs(i) / (float) maxScroll;
+
+        if (percentage == 1f && hiddenToolbarView) {
+            titleAppBar.setVisibility(View.GONE);
+            hiddenToolbarView = !hiddenToolbarView;
+        } else if (percentage < 1f && hiddenToolbarView) {
+            titleAppBar.setVisibility(View.VISIBLE);
+            hiddenToolbarView = !hiddenToolbarView;
+        }
     }
 }
